6fcf6eee2613649f18588eb4a0f5229aaa087428
Paulstar200 <vitalispaul48@live.com>
2020-01-23 20:11:32 +0300

    First solution

--- CreatePhoneNumber/index.js
+++ CreatePhoneNumber/index.js
@@ -3,3 +3,28 @@
  * that returns a string of those numbers in the form of a phone number.
  * Example: createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
  */
+
+function createPhoneNumber(numbers) {
+    let phoneNumber = '';
+    let openParen = '(';
+    let closingParen = ') ';
+    let hiphen = '-';
+
+
+    for (let number in numbers) {
+        phoneNumber += numbers[number]
+    }
+    let newPhoneNumber = openParen += phoneNumber;
+    let thirdIndex = newPhoneNumber.charAt(4);
+    let firstPart = newPhoneNumber.split(thirdIndex);
+    let numberWithoutHiphen = firstPart[0] + closingParen + thirdIndex + firstPart[1];
+    let ninthIndex = numberWithoutHiphen.charAt(9);
+    let secondPart = numberWithoutHiphen.split(ninthIndex);
+    let numberWithHiphen = secondPart[0] + hiphen + ninthIndex + secondPart[1];
+    return numberWithHiphen;
+
+}
+
+let myNumbers = createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+
+console.log(myNumbers);
