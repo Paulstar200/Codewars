f955ce376903c58c99053393b6dd7c1171400aa5
Paulstar200 <vitalispaul48@live.com>
2020-02-03 16:26:37 +0300

    Add Create Phone number logic

--- CreatePhoneNumber/index.js
+++ CreatePhoneNumber/index.js
@@ -5,26 +5,43 @@
  */
 
 function createPhoneNumber(numbers) {
+    // String for holding the phone numbers
     let phoneNumber = '';
-    let openParen = '(';
-    let closingParen = ') ';
-    let hiphen = '-';
-
 
     for (let number in numbers) {
         phoneNumber += numbers[number]
     }
-    let newPhoneNumber = openParen += phoneNumber;
-    let thirdIndex = newPhoneNumber.charAt(4);
-    let firstPart = newPhoneNumber.split(thirdIndex);
-    let numberWithoutHiphen = firstPart[0] + closingParen + thirdIndex + firstPart[1];
-    let ninthIndex = numberWithoutHiphen.charAt(9);
-    let secondPart = numberWithoutHiphen.split(ninthIndex);
-    let numberWithHiphen = secondPart[0] + hiphen + ninthIndex + secondPart[1];
-    return numberWithHiphen;
 
+    // Add opening parenthesis to beginning of the string
+    let newPhoneNumber = '(' + phoneNumber;
+    let arrayOfNumbers = newPhoneNumber.split('');
+
+    // Get the first part of the string before the closing parenthesis position
+    let firstPart = arrayOfNumbers.slice(0, 4);
+    firstPart.push(')')
+    let stringifiedfirstPart = firstPart.join('');
+
+    // Get the second part of the string after the closing parenthesis position
+    let secondPart = arrayOfNumbers.slice(4);
+    let numberWithoutHiphen = secondPart.slice(0, 3); // Get the numbers before the hiphen position
+    numberWithoutHiphen.push('-')
+    let stringifiedSecondPart = numberWithoutHiphen.join('');
+
+    // Get the last part of the string which is after the hiphen
+    let lastpart = secondPart.slice(3);
+    let stringedLastPart = lastpart.join('')
+
+    // Return the final string and insert a space between the first part and the second part
+    return stringifiedfirstPart + ' ' + stringifiedSecondPart + stringedLastPart;
 }
 
-let myNumbers = createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+function alternativeSolution(numbers) {
+    numbers = numbers.join('');
+    return '(' + numbers.substring(0, 3) + ') ' + numbers.substring(3, 6) + '-' + numbers.substring('6');
+}
+
+let myNumbers = createPhoneNumber([1, 2, 3, 5, 1, 1, 8, 2, 9, 1]);
+let alternative = alternativeSolution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
 console.log(myNumbers);
+console.log(alternative);
